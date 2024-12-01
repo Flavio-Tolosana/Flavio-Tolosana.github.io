ebba5aee7e5b23a9061ad35bcdcbb52f6ec9caf3
Flavio-Tolosana <flavio.tolosana@gmail.com>
2024-12-01 16:16:39 +0000

    Cambios vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flavio-tolosana.github.io/'
+  // base: '/flavio-tolosana.github.io/'
+  base: '/'
 })
